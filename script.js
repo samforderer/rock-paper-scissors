b28c0091c01217d27cb2d3a5318e31e235d250fa
Sam Forderer <samforderer@Sams-iMac.local>
2022-08-17 12:26:13 -0400

    created game function to play a 5 round winner takes all mode

--- script.js
+++ script.js
@@ -2,24 +2,57 @@
 function getComputerChoice() {
   rockPaperScissors = ['rock', 'paper', 'scissors']
   x = Math.floor(Math.random() * 3);
-  computerSelection = rockPaperScissors[x];
+  choice = rockPaperScissors[x];
+  return choice;
 }
 
-function playSingleRound(playerSelection, computerSelection) {
-  if (playerSelection === 'rock' && computerSelection === 'paper') { 
-    return "you win!";
+// compare the players choice with the computers choice and determin the winner of the round
+function playRound(playerSelection, computerSelection) {
+  console.log(playerSelection, computerSelection)
+  if (playerSelection === 'rock' && computerSelection === 'scissors') { 
+    playerScore++;
+    return "you win! rock beats paper!";
   } else if (playerSelection === 'paper' && computerSelection === 'rock') {
-    return "you win!";
+    let playerScore =+ 1;
+    return "you win! paper beats rock!";
+  } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
+    let playerScore =+ 1;
+    return "you win! scissors beats paper!";
+  } else if (computerSelection === 'rock' && playerSelection === 'scissors') { 
+    let computerScore =+ 1;
+    return "you lose! rock beats paper!";
+  } else if (computerSelection === 'paper' && playerSelection === 'rock') {
+    let computerScore =+ 1;
+    return "you lose! paper beats rock!";
+  } else if (computerSelection === 'scissors' && playerSelection === 'paper') {
+    let computerScore =+ 1;
+    return "you lose! scissors beats paper!";
+  } else if (playerSelection === computerSelection) {
+    return "it was a tie!";
   }
-
-
 }
 
 
 
+function game() {
+  let playerScore = 0;
+  let computerScore = 0;
 
+  for (let i = 0; i < 5; i++) {
+    let playerSelection = prompt('What is your choice?').toLowerCase();
+    let computerSelection = getComputerChoice();
+    result = playRound(playerSelection, computerSelection);
+    console.log(result);
 
-console.log(getComputerChoice());
-let playerSelection = prompt('What is your choice?');
+    //Check to see if the result was a win or a loss increment player score if win increment computer score if loss
+    if (result.substr(0, 5) == 'you w') {
+      playerScore++;
+    } else if (result.substr(0, 5) == 'you l') {
+      computerScore++;
+    }
+  }
+  console.log('the final score was ', playerScore, computerScore);
+  (playerScore > computerScore) ? console.log('You beat the computer!') : console.log('The computer has bested you, mate!');
+}
 
-playSingleRound(playSingleRound, computerSelection);
+game();
